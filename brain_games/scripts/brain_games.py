58ff7889fa173029150db5e68bffd92e0b8c47ce
Vlad Borovkov <vladborovkov747@gmail.com>
2021-03-03 22:50:29 +0300

    Modification_2

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+import prompt
+
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print('Hello, ' + name + '!')
+
 
 def brain_games():
-    print ('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
+
 
 def main():
     brain_games()
+    welcome_user()
+
 
 if __name__ == '__main__':
     main()
