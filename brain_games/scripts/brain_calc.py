205a140b631e5e3989a257c78f6f3a6a7d356956
Vlad Borovkov <vladborovkov747@gmail.com>
2021-03-13 23:57:27 +0300

    creature brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from brain_games.games import start_calc
+from brain_games.game_structure import start_game
+
+
+def main():
+    start_game(start_calc)
+
+
+if __name__ == '__main__':
+    main()
