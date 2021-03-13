3eb6c2a55c51fa77233015e4ee37d49169b378a6
Vlad Borovkov <vladborovkov747@gmail.com>
2021-03-14 00:01:59 +0300

    creature brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from brain_games.games import start_progression
+from brain_games.game_structure import start_game
+
+
+def main():
+    start_game(start_progression)
+
+
+if __name__ == '__main__':
+    main()
